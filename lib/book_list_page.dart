4d10690f260d9f8af2536ca34f49d066099698f2
Kilyusan <kih2so3@gmail.com>
2022-04-07 05:29:11 +0900

    4/7 test_providerをGithubに上げた

---
+++ lib/book_list_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BookListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('本一覧'),
+      ),
+      body: Center(
+        // in the middle of the parent.
+        child: Text(
+          '本一覧',
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:test_provider/book_list_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();//options: DefaultFirebaseConfig.platformOptions
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TestProvider',
+      home: BookListPage(),
+    );
+  }
+}
+
